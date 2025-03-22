f37f8949fcaaf139fd71588686065580390e6cf6
Dakota_Olh <ochaiuk2006@gmail.com>
2025-03-23 00:37:44 +0200

    Connect to Firebase and add simple authentication

--- app/src/main/java/com/example/bookvoyager/LoginActivity.java
+++ app/src/main/java/com/example/bookvoyager/LoginActivity.java
@@ -7,24 +7,47 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText signupEmail, signupPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Window window = getWindow();
-        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        window.setStatusBarColor(Color.TRANSPARENT);
+        fullScreen();
+
+        auth = FirebaseAuth.getInstance();
+        signupEmail = findViewById(R.id.enterEmail);
+        signupPassword = findViewById(R.id.enterPassword);
+
+//        if(auth.getCurrentUser() != null){
+//            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//            startActivity(intent);
+//            finish();
+//        }
+
 
         TextView registrationLink = findViewById(R.id.registrationTransitionLink);
         registrationLink.setOnClickListener(new View.OnClickListener() {
@@ -39,9 +62,42 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                String user = signupEmail.getText().toString().trim();
+                String pass = signupPassword.getText().toString().trim();
+
+                if(user.isEmpty()){
+                    signupEmail.setError("is empty");
+                }
+                else if(pass.isEmpty()){
+                    signupPassword.setError("is empty");
+                }
+                else {
+                    auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(LoginActivity.this, "is Successful", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                            else {
+                                Toast.makeText(LoginActivity.this, "isn't Successful", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                }
             }
         });
+
+
     }
+
+    private void fullScreen(){
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        window.setStatusBarColor(Color.TRANSPARENT);
+    }
+
 }
--- app/src/main/java/com/example/bookvoyager/MainActivity.java
+++ app/src/main/java/com/example/bookvoyager/MainActivity.java
@@ -13,6 +13,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
